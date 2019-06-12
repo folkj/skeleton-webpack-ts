0752a05a3f03dec93bfa681e907d441c9d9c384f
Justin Folk <Justin.Folk@gilead.com>
2019-06-12 09:03:40 -0700

    changed webpack and package json

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 
-import 'core-js'
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Hello from './my-module'
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,44 @@
 const path = require('path');
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = {
-	entry: "./src/index.tsx",
+	entry: {
+		Editor: "./src/index.tsx",
+		Home: "./Homepage/index.tsx"
+	},
 	output: {
-		filename: "App.js",
+		filename: "[name].bundle.js",
 		path: path.resolve(__dirname, "public")
 	},
 
 	resolve: {
 		// Add `.ts` and `.tsx` as a resolvable extension.
-		extensions: [".ts", ".tsx", ".js"]
+		extensions: [".ts", ".tsx", ".js", ".jsx"]
 	},
 
+	// plugins: [
+	// 	new BundleAnalyzerPlugin()
+	// ],
+	
 	module: {
 		rules: [
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url-loader'
+			},
+			{ test: /\.png$/, loader: "url-loader?mimetype=image/png" },
 			{ test: /\.tsx?$/, loader: "ts-loader" },
-			{ test: /\.css$/, loader: "css-loader" },
-			{ test: /\.js$/, loader: "babel-loader" }
+			{ test: /\.css$/, use: ["style-loader", "css-loader"] },
+			{
+				test: /\.jsx?$/,
+				exclude: [/node_modules/],
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"]
+					}
+				}],
+				
+			}
 		]
 	}
 }
